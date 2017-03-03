92e38e80dd593b9f80c2d5f0463487547ad9ab95
Gustavo Lima <gustavomdcl@gmail.com>
2017-03-02 21:59:27 -0300

    Teste mysqli_select_db

--- www/backend/seguranca.php
+++ www/backend/seguranca.php
@@ -16,7 +16,7 @@
 	
 	if ($_SG['conectaServidor'] == true) {
 		$_SG['link'] = mysqli_connect($_SG['servidor'], $_SG['usuario'], $_SG['senha']) or die("MySQL: Não foi possível conectar-se ao servidor [".$_SG['servidor']."].");
-		mysql_select_db($_SG['banco'], $_SG['link']) or die("MySQL: Não foi possível conectar-se ao banco de dados [".$_SG['banco']."].");
+		mysqli_select_db($_SG['banco'], $_SG['link']) or die("MySQL: Não foi possível conectar-se ao banco de dados [".$_SG['banco']."].");
 	}
 	
 	if ($_SG['abreSessao'] == true) {
