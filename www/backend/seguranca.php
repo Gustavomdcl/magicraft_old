3e62ce7736c3ff02256653cdee72c24f73c8ad29
Gustavo Lima <gustavomdcl@gmail.com>
2017-03-02 22:00:18 -0300

    Retorno

--- www/backend/seguranca.php
+++ www/backend/seguranca.php
@@ -15,8 +15,8 @@
 	$_SG['paginaLogin'] = 'index.php';        
 	
 	if ($_SG['conectaServidor'] == true) {
-		$_SG['link'] = mysqli_connect($_SG['servidor'], $_SG['usuario'], $_SG['senha']) or die("MySQL: Não foi possível conectar-se ao servidor [".$_SG['servidor']."].");
-		mysqli_select_db($_SG['banco'], $_SG['link']) or die("MySQL: Não foi possível conectar-se ao banco de dados [".$_SG['banco']."].");
+		$_SG['link'] = mysql_connect($_SG['servidor'], $_SG['usuario'], $_SG['senha']) or die("MySQL: Não foi possível conectar-se ao servidor [".$_SG['servidor']."].");
+		mysql_select_db($_SG['banco'], $_SG['link']) or die("MySQL: Não foi possível conectar-se ao banco de dados [".$_SG['banco']."].");
 	}
 	
 	if ($_SG['abreSessao'] == true) {
