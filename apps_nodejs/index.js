74f88cc23679afb908e28ebc27a29dddba61d08f
Gustavo Lima <gustavomdcl@gmail.com>
2017-03-19 19:01:00 -0300

    novo path

--- apps_nodejs/index.js
+++ apps_nodejs/index.js
@@ -1,6 +1,6 @@
 var app = require('express')();  
 var http = require('http').createServer(app);  
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, { path: '/play/socket.io'});
 
 app.get('/', function(req, res){  
   res.sendFile(__dirname + '/index.html');
