0a6cb9cec97cbfc22fd1ec0fb73e7f29c732a8d9
Gustavo Lima <gustavomdcl@gmail.com>
2017-03-19 17:58:53 -0300

    teste final

--- apps_nodejs/index.js
+++ apps_nodejs/index.js
@@ -1,6 +1,6 @@
 var app = require('express')();  
 var http = require('http').Server(app);  
-var io = require('socket.io')(http);
+var io = require('socket.io')(http.listen(21288));
 
 app.get('/', function(req, res){  
   res.sendFile(__dirname + '/index.html');
