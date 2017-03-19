a37ad431be9a1287df8389281d67ecd170e060a7
Gustavo Lima <gustavomdcl@gmail.com>
2017-03-19 18:55:42 -0300

    teste /play/

--- apps_nodejs/index.js
+++ apps_nodejs/index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);  
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){  
+app.get('/play/', function(req, res){  
   res.sendFile(__dirname + '/index.html');
 });
 
