3a22c27817a15f57ea1c03c244db8c29592cfde1
Gustavo Lima <gustavomdcl@gmail.com>
2017-03-25 10:37:40 -0300

    Login iniciado

--- apps_nodejs/index.js
+++ apps_nodejs/index.js
@@ -1,20 +1,62 @@
-var app = require('express')();  
-var http = require('http').createServer(app);  
-//var io = require('socket.io')(http, { path: '/play/socket.io'});
-var io = require('socket.io')(http);
+/* ==== VARIABLES ==== */
 
-app.get('/', function(req, res){  
-  res.sendFile(__dirname + '/index.html');
+var app = require('express')();  
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'mysql.magicraft.life',
+  user     : 'magicraft',
+  password : 'P0tt3rPlay666',
+  database : 'magicraft'
+});
+var crypto = require('crypto'); //senhas https://gist.github.com/kitek/1579117
+
+/* ==== MYSQL ==== */
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
 });
 
-//Storage
+function userLogin(userEmail, userPassword){
+  userPassword = crypto.createHash('md5').update(userPassword).digest("hex"); //senha md5
+  console.log(userPassword);
+  var userSelection = "SELECT * FROM `PP_USER` WHERE `email` = '"+userEmail+"' AND `password` = '"+userPassword+"' LIMIT 1";
+  connection.query(userSelection, function (error, results, fields) {
+    if (error) {
+      throw error;
+    } else {
+      if(results!=''){
+        //Login Succeeded
+        console.log(
+          'The results are: '+
+          'id: '+ results[0].id+
+          '; name: '+ results[0].name+
+          '; email: '+ results[0].email+
+          '; user: '+ results[0].user + ';'
+        );
+      } else {
+        //Login Failed
+        console.log('Login Failed');
+      }
+    }
+  });
+}
+
+userLogin('gustavomdcl@gmail.com', '1234sucesso');
+ 
+//connection.end();
+
+/* ==== SocketIo ==== */
+
+//User Storage
 var allConnectedClients = Object.keys(io.sockets.connected);
 var currentConnections = {};
 io.sockets.on('connection', function (client) {
     for (var i = 0, len = allConnectedClients.length; i < len; i++) {
-      //SOCKET ID = console.log('coconut '+i+' = '+allConnectedClients[i]);
-      //SOCKET ID DATA = console.log('coconut '+i+' data = '+currentConnections[allConnectedClients[i]].data);
-      //Vai emitir somente para esse novo client/socket
       client.emit('first user', currentConnections[allConnectedClients[i]].clientData);
     }
     client.emit('start placement', 'start');
@@ -22,8 +64,6 @@ io.sockets.on('connection', function (client) {
     client.on('data', function (somedata) {
         currentConnections[client.id].clientData = somedata; 
         allConnectedClients = Object.keys(io.sockets.connected);
-        //SOCKET ID = console.log('socket: '+currentConnections[client.id].socket.id);
-        //SOCKET ID DATA = console.log('data: '+currentConnections[client.id].clientData);
     });    
     client.on('disconnect', function() {
         io.sockets.emit('logout user', currentConnections[client.id].clientData);
@@ -32,72 +72,19 @@ io.sockets.on('connection', function (client) {
     });
 });
 
+//User's Movement
 io.on('connection', function(socket){  
-  console.log('um usuario conectou');
-  socket.on('disconnect', function(){
-    console.log('usuario desconectou');
-  });
-  socket.on('user change', function(user_data){  
-    console.log('message: ' + user_data);
+  socket.on('user change', function(user_data){
     io.sockets.emit('user change', user_data);
   });
 });
 
-/*http.listen(process.env.PORT_INDEX, function(){  
-  console.log('Server running at :'+process.env.PORT_INDEX);
-});*/
+/* ==== Cria Servidor na porta ==== */
+
+app.get('/', function(req, res){  
+  res.sendFile(__dirname + '/index.html');
+});
 
 http.listen('21288', function(){  
   console.log('Server running at :'+'21288');
 });
-
-/* ==== MYSQL ==== */
-
-//https://www.npmjs.com/package/mysql
-
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'mysql.magicraft.life',
-  user     : 'magicraft',
-  password : 'P0tt3rPlay666',
-  database : 'magicraft'
-});
- 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
- 
-  console.log('connected as id ' + connection.threadId);
-});
-
-var userEmail = 'gustavomdcl@gmail.com'
-var userSelection = "SELECT * FROM PP_USER WHERE email = '"+userEmail+"'";
-
-connection.query(userSelection, function (error, results, fields) {
-  if (error) {
-    throw error;
-  } else {
-    if(results!=''){
-      console.log(
-        'The results * from the table PP_USER where the email is the userEmail are: '+
-        'id: '+ results[0].id+
-        '; name: '+ results[0].name+
-        '; email: '+ results[0].email+
-        '; user: '+ results[0].user + ';'
-      );
-    }
-  }
-});
- 
-connection.end();
-
-/*var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node\n');
-  res.end('I am a wizard\n');
-  console.log('gordin');
-}).listen(process.env.PORT_INDEX);
-console.log('Server running at :'+process.env.PORT_INDEX);*/
